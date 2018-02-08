a3e89c3401fb6eb3890e96a201c464036b8c68f6
Olga <olgayakimovich87@gmail.com>
2018-02-08 08:53:12 +0300

    fixed code

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 'use strict'
-let getData = () => {
     let lastName = prompt('Введите вашу фамилию');
-
+    while(lastName == '' || lastName == null){
+        lastName = prompt('Введите вашу фамилию');
+    } 
     let firstName = prompt('Введите ваше имя');
-    
+    while (firstName == '' || firstName == null){
+        firstName = prompt('Введите ваше имя');
+    }
     let patronymic = prompt('Введите ваше отчество');
-
+    while (patronymic == '' || patronymic == null){
+        patronymic = prompt('Введите ваше отчество');
+    }
     let age = parseInt(prompt('Ваш возраст:'));
-    if (isNaN(age)){
-        age = parseInt(prompt('Ваш возраст (только цифры):'));
+    while (isNaN(age) || age == ''){
+        age = parseInt(prompt('Ваш возраст:'));
     }
     let gender = confirm('Ваш пол мужской? Если так, нажмите "OK", если женский нажмите "Отмена"');
     
@@ -24,14 +29,7 @@ let getData = () => {
         gender = 'женский';
         answer = (retireWomen > age)? 'нет': 'да';
     }
-    if (lastName == '' || firstName == '' || patronymic == '' || age == '' || lastName == null || firstName == null || patronymic == null || isNaN(age)){
-        alert('Некоторые поля были пустыми. Заполните, пожалуйста, анкету');
-        getData();
-    } else {
-        alert(`Ваше ФИО: ${lastName} ${firstName} ${patronymic} \n Ваш возраст в годах: ${age} \n Ваш возраст в днях: ${age*365} \n Через 5 лет вам будет: ${age+5} \n Ваш пол: ${gender} \n Вы на пенсии: ${answer}`);
-    }
-    
-    
-}
-getData();
+  
+    alert(`Ваше ФИО: ${lastName} ${firstName} ${patronymic} \n Ваш возраст в годах: ${age} \n Ваш возраст в днях: ${age*365} \n Через 5 лет вам будет: ${age+5} \n Ваш пол: ${gender} \n Вы на пенсии: ${answer}`);
+
 
